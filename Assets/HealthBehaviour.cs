40c2cc71bd62496b41789568865041ce95817b9b
rafaelalma92 <rafaelalma92@gmail.com>
2020-11-25 20:26:30 +0100

    Solution

---
+++ Assets/HealthBehaviour.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class HealthBehaviour : MonoBehaviour
+{
+    public UnityEvent<int> OnChanged;
+
+    [SerializeField] private int maxHealth;
+
+    private int currentHealth;
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+        HealthChanged(currentHealth);
+    }
+
+    public void DoDamage(int damageAmount)
+    {
+        if (currentHealth > 0)
+        {
+            currentHealth = Math.Max(currentHealth - damageAmount, 0);
+
+            HealthChanged(currentHealth);
+        }
+    }
+
+    public void DoHealing(int healingAmount)
+    {
+        if (currentHealth < maxHealth)
+        {
+            currentHealth = Math.Min(currentHealth + healingAmount, maxHealth);
+
+            HealthChanged(currentHealth);
+        }
+    }
+
+    private void HealthChanged(int health)
+    {
+        OnChanged.Invoke(health);
+    }
+}
